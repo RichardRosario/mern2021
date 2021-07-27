0c736771743af5058b4869572224f33601cd7e0b
unknown <catrosits@gmail.comgit config --global user.name>
2021-07-27 23:43:28 +0800

    dispatching actions through redux hooks

--- server/index.js
+++ server/index.js
@@ -7,12 +7,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
 //connect to db
 const CONNECTION_URL =
 	"mongodb+srv://spaceko:spaceko123@cluster0.qa7hu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
