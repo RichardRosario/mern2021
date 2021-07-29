4af0abf902492744f9b489a219a92edc5347f4a4
unknown <catrosits@gmail.comgit config --global user.name>
2021-07-30 00:01:28 +0800

    displaying the post data

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React from "react";
-
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
+
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
@@ -9,13 +10,25 @@ const Posts = () => {
 	const classes = useStyles();
 
 	console.log(posts);
+
 	return (
 		//add react fragments
-		<>
-			<h1 className={classes.mainContainer}>POSTS</h1>
-			<Post />
-			<Post />
-		</>
+		!posts.length ? (
+			<CircularProgress />
+		) : (
+			<Grid
+				className={classes.container}
+				container
+				alignItems='stretch'
+				spacing={3}
+			>
+				{posts.map(post => (
+					<Grid key={post._id} item xs={12} sm={6}>
+						<Post post={post} />
+					</Grid>
+				))}
+			</Grid>
+		)
 	);
 };
 
